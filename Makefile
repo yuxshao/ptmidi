34e64c54bbb830bcfc04653ff7c28785c1d49775
Steven <ys2833@columbia.edu>
2017-08-31 05:02:04 -0400

    in data structure

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 main: *.cpp
-	g++ -g main.cpp -o main -L../pxtone/lib -L../midifile/lib -lpxtone -lmidifile -I../pxtone -I../midifile/include
+	g++ -g -std=c++1z main.cpp -o main -L../pxtone/lib -L../midifile/lib -lpxtone -lmidifile -I../pxtone -I../midifile/include
 
 clean:
 	rm -f main
--- main.cpp
+++ main.cpp
@@ -2,14 +2,30 @@
 #include "pxtone/pxtnService.h"
 #include <cmath>
 #include <iostream>
-#include <queue>
-
-struct Note {
-  int vel, key;
-};
+#include <map>
 
 struct PitchEvent {
-  int pitch, time;
+  int pitch, clock, end_clock;
+};
+
+template <typename T> using Historical = std::map<int, T>;
+
+template <typename T> auto at_time(Historical<T> hist, int time) {
+  auto it = hist.upper_bound(time);
+  if (it == hist.begin())
+    return hist.end();
+  else
+    return --it;
+}
+
+struct Press {
+  int vel, length;
+};
+
+struct Unit {
+  Historical<Press> presses;
+  Historical<int> notes, volume, porta, voice, group, pan_v, pan_t;
+  Historical<float> tuning;
 };
 
 void print_event(const EVERECORD *p) {
@@ -42,80 +58,122 @@ int main(int argc, char **args) {
 
   // Create other tracks from units
   for (int i = 0; i < pxtn.Unit_Num(); ++i) {
-    const pxtnUnit &unit = *pxtn.Unit_Get(i);
-    midifile.addTrackName(i + 1, 0, unit.get_name_buf(nullptr));
-    // configure cc6 to set pitch bend range
-    midifile.addController(i + 1, 0, i + 1, 100, 0);
-    midifile.addController(i + 1, 0, i + 1, 101, 0);
-    // 104 = default volume
-    midifile.addController(i + 1, 0, i + 1, 11, 104);
   }
 
-  // Note: assuming records are non-decreasing in clock
   int last_clock = -1;
-  std::queue<const EVERECORD *> records;
-  std::vector<Note> notes(pxtn.Unit_Num());
-  // std::vector<PitchEvent> pitches(pxtn.Unit_Num());
+  std::vector<Unit> units(pxtn.Unit_Num());
   for (const EVERECORD *p = pxtn.evels->get_Records(); p; p = p->next) {
-    // process velocities & keys for this instant first.
-    if (p->clock < last_clock) {
-      std::cerr << "records not in time-ascending order" << std::endl;
-      return -1;
-    } else if (p->clock > last_clock) {
-      for (; !records.empty(); records.pop()) {
-        const EVERECORD *p = records.front();
-        int track = p->unit_no + 1;
-        int channel = p->unit_no + 1;
-        if (channel == 9) ++channel; // to avoid drums
-        switch (p->kind) {
-        case EVENTKIND_ON: {
-          int key = notes[p->unit_no].key / 256 - 27;
-          int vel = notes[p->unit_no].vel;
-          midifile.addNoteOn(track, p->clock, channel, key, vel);
-          midifile.addNoteOff(track, p->clock + p->value, channel, key);
-          break;
-        }
-
-        case EVENTKIND_VOLUME:
-          // 11 = Expression MSB, 43 = Expression LSB
-          midifile.addController(track, p->clock, channel, 11, p->value);
-          break;
-
-        case EVENTKIND_PAN_VOLUME:
-          // TODO: do some function of time and volume
-          // 10 = Pan MSB, 42 = Pan LSB
-          midifile.addController(track, p->clock, channel, 10, p->value);
-          break;
-
-        case EVENTKIND_TUNING: {
-          // TODO: extend this once you do portamentos
-          // 6 = Pitch bend range
-          float value = reinterpret_cast<const float &>(p->value);
-          double note_offset = std::log2(value) * 12;
-          int pitch_bend_range =
-              std::max<int>(1, std::ceil(std::abs(note_offset)));
-          double abs_offset = note_offset / pitch_bend_range;
-          midifile.addController(track, p->clock, channel, 6, pitch_bend_range);
-          midifile.addPitchBend(track, p->clock, channel, abs_offset);
-          break;
-        }
-
-        default:
-          break;
-        }
-      }
-      last_clock = p->clock;
-    }
     switch (p->kind) {
-    case EVENTKIND_VELOCITY:
-      notes[p->unit_no].vel = p->value;
+    case EVENTKIND_ON: {
+      auto &hist = units[p->unit_no].presses;
+      hist.emplace(p->clock, Press{}).first->second.length = p->value;
       break;
-    case EVENTKIND_KEY:
-      notes[p->unit_no].key = p->value;
-      break;
-    default:
-      records.push(p);
     }
+
+    case EVENTKIND_VELOCITY: {
+      auto &hist = units[p->unit_no].presses;
+      hist.emplace(p->clock, Press{}).first->second.vel = p->value;
+      break;
+    }
+
+    case EVENTKIND_KEY:
+      units[p->unit_no].notes.emplace(p->clock, p->value / 256 - 27);
+      break;
+
+    case EVENTKIND_VOLUME:
+      units[p->unit_no].volume.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_PAN_VOLUME:
+      units[p->unit_no].pan_v.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_PAN_TIME:
+      units[p->unit_no].pan_t.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_PORTAMENT:
+      units[p->unit_no].porta.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_VOICENO:
+      units[p->unit_no].voice.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_GROUPNO:
+      units[p->unit_no].group.emplace(p->clock, p->value);
+      break;
+
+    case EVENTKIND_TUNING:
+      auto &hist = units[p->unit_no].tuning;
+      hist.emplace(p->clock, reinterpret_cast<const float &>(p->value));
+      break;
+
+    default:
+      std::cout << "warning: unhandled event - ";
+      print_event(p);
+      break;
+    }
+  }
+
+  for (int i = 0; i < pxtn.Unit_Num(); ++i) {
+    int track = i + 1;
+    int channel = (i + 1 >= 10 ? i + 2 : i + 1); // skip the drum channel
+
+    const pxtnUnit &unit = *pxtn.Unit_Get(i);
+    midifile.addTrackName(track, 0, unit.get_name_buf(nullptr));
+    // configure cc6 to set pitch bend range
+    midifile.addController(track, 0, channel, 100, 0);
+    midifile.addController(track, 0, channel, 101, 0);
+    // 104 = default volume
+    midifile.addController(track, 0, channel, 11, 104);
+    midifile.addPitchBend(track, 0, channel, 0);
+
+    for (const auto & [ time, volume ] : units[i].volume)
+      midifile.addController(track, time, channel, 11, volume);
+
+    for (const auto & [ time, pan_v ] : units[i].pan_v)
+      midifile.addController(track, time, channel, 10, pan_v);
+
+    for (const auto & [ time, press ] : units[i].presses) {
+      auto key_it = at_time(units[i].notes, time);
+      // 48 is a default
+      int key = (key_it == units[i].notes.end() ? 48 : key_it->second);
+      midifile.addNoteOn(track, time, channel, key, press.vel);
+      midifile.addNoteOff(track, time + press.length, channel, key);
+    }
+
+    for (const auto & [ time, tuning ] : units[i].tuning) {
+      // 6 = Pitch bend range
+      float value = reinterpret_cast<const float &>(tuning);
+      double note_offset = std::log2(value) * 12;
+      int pitch_bend_range = std::max<int>(1, std::ceil(std::abs(note_offset)));
+      double abs_offset = note_offset / pitch_bend_range;
+      midifile.addController(track, time, channel, 6, pitch_bend_range);
+      midifile.addPitchBend(track, time, channel, abs_offset);
+    }
+
+    /*case EVENTKIND_KEY: {
+    // key has not yet been updated. create pitch bend line
+    int track = p->unit_no + 1;
+    int channel = p->unit_no + 1;
+    int start_pitch = pitches[p->unit_no].pitch;
+    int start_clock = pitches[p->unit_no].clock;
+    int port_time = 240; // half a beat
+    int pitch_diff = notes[p->unit_no].key - start_pitch;
+    int pitch_bend_range =
+        std::max<int>(1, std::ceil(std::abs(pitch_diff / 256)));
+    midifile.addController(track, p->clock, channel, 6, pitch_bend_range);
+    for (int i = 0; i <= std::min<int>(port_time, p->clock); i += 10) {
+      double abs_offset =
+          pitch_diff * i / (port_time * 256. * pitch_bend_range);
+      std::cout << abs_offset << std::endl;
+      midifile.addPitchBend(track, start_clock + i, channel, abs_offset);
+    }
+
+    notes[p->unit_no].key = p->value;
+    break;
+  }*/
   }
 
   midifile.sortTracks();
